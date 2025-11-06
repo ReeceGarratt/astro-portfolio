d0d1131f0811dadd512461244980545dd1702038
Reece Garratt <24543342+ReeceGarratt@users.noreply.github.com>
2025-11-06 23:20:54 +0200

    contact us page UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
+const isProduction = process.env.NODE_ENV === 'production' || process.env.GITHUB_ACTIONS === 'true';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cassandragarratt.com',
-  base: '/',
+  site: isProduction ? 'https://reecegarratt.github.io' : 'http://localhost:3000',
+  base: isProduction ? '/astro-portfolio/' : '/',
   integrations: [
     tailwind(),
     react(),
